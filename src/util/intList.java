0405db628e7ce0f8733ca4902eb740ec90435a34
nhn1030 <shgusska12@gmail.com>
2023-08-17 15:37:12 +0900

    정수형 콜렉션 구현

--- src/util/intList.java
+++ src/util/intList.java
@@ -3,22 +3,38 @@ package util;
 public class intList {
 
     private int[] nums;
+    // 정수 배열 멤버 변수 nums를 정의함
     private int current;
+    // 정수 변수인 current를 선언하여 현재 배열에 저장된 요소의 수를 추적
     public intList() {
         nums = new int[3];
         current = 0;
+
+        // intList 생성자 메서드. nums를 3개의 정수 배열 변수로 초기화하고, current 값을 0으로 초기화 함
     }
     public void add(int num) {
         nums[current] = num;
         current++;
+
+        // 정수 값을 받아 배열에 추가하는 메서드임
+        // nums의 [current] 번째에 해당하는 위치에 num을 저장하게 됨
+        // 저장한 후 current 값을 증가 시켜 저장소 위치를 이동시킴
     }
 
     public void clear() {
         current = 0;
+        // clear 메서드는 배열 내의 모든 요소를 없애주는 기능을 하는 것 같지만 그렇지않음
+        // 배열의 인덱스 정보를 나타내는 [current]를 0의 위치로 초기화 함으로
+        // 배열의 인덱스가 0번째 부터 다시 값을 받을 수 있게함
     }
 
     public int size() {
         return current;
+
+        // size메서드는 current를 반환하여, 현재까지 생성된 current의 인덱스 정보를 통해
+        // 배열의 size를 계산해주는 메서드임.
+        // size는 current와 동일함
+
     }
 
     public int get(int index) {
@@ -27,4 +43,12 @@ public class intList {
             throw new IndexOutOfBoundsException();
         return nums[index];
     }
+
+    // get은 정수 배열의 인덱스 요소를 가져오는 메서드
+    // 외부에서 받아온 매개 값을 이용하여
+    // 만약, current 값이 매개변수인 index 보다 작다면
+    // IndexOutOfBoundsException 예외를 던진다.
+    // 아니라면 nums[index] index번째의 요소를 return한다.
+
+
 }
--- src/util/program.java
+++ src/util/program.java
@@ -3,21 +3,39 @@ package util;
 public class program {
     public static void main(String[] args) {
         intList list = new intList();
+        // intList 자료형의 참조 변수인 list를 생성
+
         list.add(3);
+        // 인스턴스 메서드 add 사용 0번째 위치에 3을 저장
+
         list.add(5);
+        // 인스턴스 메서드 add 사용 1번째 위치에 5를 저장
 
 
         int size = list.size();
+        // main 함수 내부에 정수형 size를 생성 인스턴스 메서드 size의 값을 대입
+
         System.out.printf("size : %d\n", size);
+        // size의 값을 출력하는 기능 3과 5가 배열에 저장되었으니 size는 2가 나올 것
 
         list.clear();
+        // clear 메서드 사용
+
         size = list.size();
+        // 배열의 인덱스가 0번째로 초기화 되었기 때문에 0을 반환
+        // 실제로는 0, 1번째 인덱스에 3과 5가 인덱스의 요소로 저장되어 있으며,
+        // 다음 add 메서드 호출 시 새로운 값으로 덮어쓰게 될 것
+
         System.out.printf("size : %d\n", size);
 
         list.add(7);
         int num = list.get(0);
         System.out.printf("num : %d\n", num);
+
         num = list.get(2);
+        // 해당 메서드는 index 값이 list의 current 값 보다 크기 때문에
+        // IndexOutOfBoundsException 예외를 반환한다.
         System.out.printf("num : %d\n", num);
+
     }
 }
