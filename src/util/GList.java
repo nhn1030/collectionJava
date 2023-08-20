8f74cebe707d0894f7eaa44042e3305078ee26c9
nhn1030 <shgusska12@gmail.com>
2023-08-20 23:36:11 +0900

    가변길이 배열

--- src/util/GList.java
+++ src/util/GList.java
@@ -5,12 +5,27 @@ public class GList <T> {
 
     private Object[] nums;
     private int current;
+    private int capacity;
+    private int amount;
 
     public GList() {
         nums = new Object[3];
         current = 0;
+        capacity = 3;
+        amount = 5;
     }
     public void add(T num) {
+
+        if (current >= capacity ) {
+            Object [] temp = new Object[capacity + amount];
+
+            for (int i = 0; i < current; i++)
+                temp[i] = nums[i];
+            nums = temp;
+
+            capacity += amount;
+
+        }
         nums[current] = num;
         current++;
 
